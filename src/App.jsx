635cf69520dda3b6846dca4922695a73628e61af
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-06-02 23:36:07 -0300

    Api e inclusão de tsc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Counter } from './components/Counter/Counter';
 import { RepositoryList } from './components/RepositoryList/RepositoryList';
-import './styles/global.scss'
+import './styles/global.scss';
 
 export function App() {
   return (
     <>
       <RepositoryList />
-      <Counter />
+      {/* <Counter /> */}
     </>
-  )
+  );
 }
--- src/components/RepositoryList/RepositoryList.jsx
+++ src/components/RepositoryList/RepositoryList.jsx
@@ -1,19 +1,27 @@
-import { RepositoryItem } from "../RepositoryItem/RepositoryItem";
+import { useState, useEffect } from "react";
 
-const repository = {
-  name: 'Nome',
-  description: 'Descrição',
-  link: 'https://github.com/brocchirodrigo/ignite-react-01-github-explorer'
-}
+import { RepositoryItem } from "./RepositoryItem/RepositoryItem";
+import "./style.scss";
+
 
 export function RepositoryList() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/brocchirodrigo/repos')
+      .then(response => response.json())
+      .then(data => setRepos(data))
+  }, []);
+
   return (
-    <section className='repository-list'>
+    <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
+        {repos.map(repo => {
+          return <RepositoryItem key={repo.id} repository={repo} />
+        })}
       </ul>
     </section>
-  )
+  );
 }
